85c922c530b9df425bd1698ef940e4507f51f5d4
laurentkal <laurentkalfon@gmail.com>
2015-07-25 00:07:25 +0200

    commit to assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Solve function returns the inverse of a matrix, cached in makeCacheMatrix.
 
-## Write a short comment describing this function
+# makeCacheMatrix takes a matrix as an argument and returns a list of functions.
+# It sets and stores a matrix, then computes its inverse and caches it.
 
 makeCacheMatrix <- function(x = matrix()) {
+        #Initializes a cached matrix to NULL
         m <- NULL
+        
+        # Sets the value of a matrix x
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        
+        # Returns the value of the matrix x, stored in MakeCacheMatrix
         get <- function() x
+        
+        # stores the value of the input (inverse) in m  and returns this value
         setinverse <- function(inverse) m <<- inverse
         getinverse <- function() m
+        
+        # Stores these 4 functions (set, get, setinverse, getinverse in a list)
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
@@ -19,17 +28,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
+# CacheSolve calculates and returns the inverse of a matrix x or returns the inverse value (m)
+# stored in the cache function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Gets the cached value 
         m <- x$getinverse()
+        
+        # If there is a cached value, it returns it
         if (!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+        
+        # Else, it gets the matrix, calculates its inverse and stores it in the cache
         data <- x$get()
         m <- solve(data) %*% data
         x$setinverse(m)
+        
+        # Returns the inverse
         m
 }
